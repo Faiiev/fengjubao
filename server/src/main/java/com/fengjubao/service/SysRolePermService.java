0e31fb6ee412ec8104a6f2d9007fc2185f8835b6
dmzsz <dmzsz@sian.com>
2019-07-25 13:43:39 +0800

    change to jwt

--- server/src/main/java/com/fengjubao/service/SysRolePermService.java
+++
@@ -1,7 +0,0 @@
-package com.fengjubao.service;
-
-import com.fengjubao.entity.SysRolePerm;
-import com.baomidou.mybatisplus.service.IService;
-
-public interface SysRolePermService extends IService<SysRolePerm> {
-}
--- server/src/main/java/com/fengjubao/service/SysUserRoleService.java
+++
@@ -1,7 +0,0 @@
-package com.fengjubao.service;
-
-import com.fengjubao.entity.SysUserRole;
-import com.baomidou.mybatisplus.service.IService;
-
-public interface SysUserRoleService extends IService<SysUserRole> {
-}
